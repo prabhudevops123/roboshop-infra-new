e70e3794cbddd64018218d08d5dea8698046555f
Prabhu <prabh.rao@gmail.com>
2023-03-19 22:29:05 +0530

    roboshop vpc

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ module "vpc" {
   source              = "git::https://github.com/prabhudevops123/tf-module-vpc.git"
   env                 = var.env
   tags                = var.tags
-#  default_route_table = var.default_route_table
-#  default_vpc_id      = var.default_vpc_id
+  default_route_table = var.default_route_table
+  default_vpc_id      = var.default_vpc_id
 
   for_each        = var.vpc
   vpc_cidr        = each.value["vpc_cidr"]
-#  public_subnets  = each.value["public_subnets"]
-#  private_subnets = each.value["private_subnets"]
+  public_subnets  = each.value["public_subnets"]
+  private_subnets = each.value["private_subnets"]
 }
 
 
---
+++ terraform.tfvars
@@ -0,0 +1,9 @@
+tags = {
+  bu_unit_name = "ecommerce"
+  app_name     = "roboshop"
+  owner        = "ecom_bu"
+  cost_center  = 1001
+}
+
+default_vpc_id      = "vpc-0faaf433a8f781f98"
+default_route_table = "rtb-0aa3e3b81abf1aba5"
