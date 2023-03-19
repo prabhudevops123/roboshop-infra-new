499ceedb14658fc1a54d629ffa4cb6a208f53563
Prabhu <prabh.rao@gmail.com>
2023-03-19 20:02:21 +0530

    roboshop vpc

--- main.tf
+++ main.tf
@@ -1,6 +1,17 @@
 module "vpc" {
+  source              = "git::https://github.com/prabhudevops123/tf-module-vpc.git"
+  env                 = var.env
+#  tags                = var.tags
+#  default_route_table = var.default_route_table
+#  default_vpc_id      = var.default_vpc_id
 
-  source = "git::https://github.com/prabhudevops123/tf-module-vpc.git"
-
-  for_each = var.vpc
+  for_each        = var.vpc
+  vpc_cidr        = each.value["vpc_cidr"]
+#  public_subnets  = each.value["public_subnets"]
+#  private_subnets = each.value["private_subnets"]
 }
+
+
+//output "vpc" {
+//  value = module.vpc
+//}
