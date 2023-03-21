6d93087ac232907546831b42e5b576a499f4872a
Prabhu <prabh.rao@gmail.com>
2023-03-21 23:11:16 +0530

    roboshop docdb jenkins

--- main.tf
+++ main.tf
@@ -20,12 +20,12 @@ module "docdb" {
 #
   for_each                = var.docdb
   engine                  = each.value["engine"]
-#  engine_version          = each.value["engine_version"]
-#  backup_retention_period = each.value["backup_retention_period"]
-#  preferred_backup_window = each.value["preferred_backup_window"]
-#  skip_final_snapshot     = each.value["skip_final_snapshot"]
-#  no_of_instances         = each.value["no_of_instances"]
-#  instance_class          = each.value["instance_class"]
+  engine_version          = each.value["engine_version"]
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  skip_final_snapshot     = each.value["skip_final_snapshot"]
+  no_of_instances         = each.value["no_of_instances"]
+  instance_class          = each.value["instance_class"]
 }
 
 #module "rds" {
