4d66fa8661f81fc197d2e903b7b6d0c9b03a2fe3
Prabhu <prabh.rao@gmail.com>
2023-03-22 17:20:26 +0530

    roboshop docdb jenkins

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
-  # Private Subnets
+  #Private Subnets
   private_subnet_ids = { for k, v in module.vpc["main"].private_subnets : k => tomap({ "id" = v.id, "availability_zone" = v.availability_zone }) }
 #  private_subnet_ids = [for k, v in module.vpc["main"].private_subnets : v.id]
   db_subnet_ids  = tolist([module.vpc["main"].private_subnets["db-az1"].id, module.vpc["main"].private_subnets["db-az2"].id])
