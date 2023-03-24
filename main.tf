4256f71ad6c7534acdbda337c42cf1cdd554878d
Prabhu <prabh.rao@gmail.com>
2023-03-25 00:47:17 +0530

    roboshop docdb jenkins

--- main.tf
+++ main.tf
@@ -99,9 +99,9 @@ module "app" {
   desired_capacity = each.value["desired_capacity"]
   max_size         = each.value["max_size"]
   min_size         = each.value["min_size"]
-  subnets          = lookup(local.subnet_ids, each.value["subnet_name"], null)
-  port             = each.value["port"]
-  allow_app_to     = lookup(local.subnet_cidr, each.value["allow_app_to"], null)
+#  subnets          = lookup(local.subnet_ids, each.value["subnet_name"], null)
+#  port             = each.value["port"]
+#  allow_app_to     = lookup(local.subnet_cidr, each.value["allow_app_to"], null)
 }
 
 output "vpc" {
