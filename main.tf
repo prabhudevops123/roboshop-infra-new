4bffef6215205f89fb93f4bf6f3d38f6db3e581a
Prabhu <prabh.rao@gmail.com>
2023-03-24 23:10:25 +0530

    roboshop docdb jenkins

--- main.tf
+++ main.tf
@@ -11,23 +11,23 @@ module "vpc" {
   private_subnets = each.value["private_subnets"]
 }
 
-module "docdb" {
-  source = "git::https://github.com/prabhudevops123/tf-module-docdb.git"
-  env    = var.env
-  tags   = var.tags
-
+#module "docdb" {
+#  source = "git::https://github.com/prabhudevops123/tf-module-docdb.git"
+#  env    = var.env
+#  tags   = var.tags
+#
+##  subnet_ids = local.db_subnet_ids
+#
+#  for_each                = var.docdb
+#  engine                  = each.value["engine"]
+#  engine_version          = each.value["engine_version"]
+#  backup_retention_period = each.value["backup_retention_period"]
+#  preferred_backup_window = each.value["preferred_backup_window"]
+#  skip_final_snapshot     = each.value["skip_final_snapshot"]
+#  no_of_instances         = each.value["no_of_instances"]
+#  instance_class          = each.value["instance_class"]
 #  subnet_ids = local.db_subnet_ids
-
-  for_each                = var.docdb
-  engine                  = each.value["engine"]
-  engine_version          = each.value["engine_version"]
-  backup_retention_period = each.value["backup_retention_period"]
-  preferred_backup_window = each.value["preferred_backup_window"]
-  skip_final_snapshot     = each.value["skip_final_snapshot"]
-  no_of_instances         = each.value["no_of_instances"]
-  instance_class          = each.value["instance_class"]
-  subnet_ids = local.db_subnet_ids
-}
+#}
 #
 #module "rds" {
 #  source = "git::https://github.com/prabhudevops123/tf-module-rds.git"
